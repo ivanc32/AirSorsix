59d3a1a5dff209b5ac6cb7145d5af205de7e4d36
Иван Цветановски <ivan.cvetanovski@students.finki.ukim.mk>
2019-05-14 14:27:06 +0200

    Partial Post Methods

--- as-backend/src/main/kotlin/com/ivanfilip/airsorsix/api/ApiController.kt
+++ as-backend/src/main/kotlin/com/ivanfilip/airsorsix/api/ApiController.kt
@@ -1,8 +1,11 @@
 package com.ivanfilip.airsorsix.api
 
+import com.ivanfilip.airsorsix.domain.Flight
 import com.ivanfilip.airsorsix.domain.Location
+import com.ivanfilip.airsorsix.domain.Plane
 import com.ivanfilip.airsorsix.service.FlightService
 import com.ivanfilip.airsorsix.service.LocationService
+import com.ivanfilip.airsorsix.service.PlaneService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,20 +14,23 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api")
 class ApiController(val flightService: FlightService,
-                    val locationService: LocationService) {
+                    val locationService: LocationService,
+                    val planeService: PlaneService) {
 
     @PostMapping("/create/location")
     fun createLocation(@RequestBody location: CreateLocationRequest): Location {
-        return locationService.addNewLocation(location.city, location.country, location.airport, location.price)
+        return locationService.addNewLocation(location.city, location.country, location.airport,
+                location.price)
     }
 
-    /*@PostMapping("/create/plane")
+    @PostMapping("/create/plane")
     fun createPlane(@RequestBody plane: CreatePlaneRequest): Plane {
-        return planeService.addNewLocation(plane.city, location.country, location.airport, location.price)
-    }*/
+        return planeService.addNewPlane(plane.manuufacturer, plane.model, plane.numberOfEconomySeat,
+                plane.numberOfBusinessSeat, plane.priceOfEconomySeat, plane.priceOfBusinessSeat)
+    }
 
-    /*@PostMapping("/create/location")
-    fun createPoll(@RequestBody location: CreateLocationRequest): Location {
-        return locationService.addNewLocation(location.city, location.country, location.airport, location.price)
-    }*/
+    @PostMapping("/create/flight")
+    fun createPoll(@RequestBody flight: CreateFlightRequest): Flight {
+        return flightService.addNewLocation(location.city, location.country, location.airport, location.price)
+    }
 }
--- as-backend/src/main/kotlin/com/ivanfilip/airsorsix/service/FlightService.kt
+++ as-backend/src/main/kotlin/com/ivanfilip/airsorsix/service/FlightService.kt
@@ -1,10 +1,27 @@
 package com.ivanfilip.airsorsix.service
 
+import com.ivanfilip.airsorsix.domain.Flight
 import com.ivanfilip.airsorsix.repository.FlightRepository
+import com.ivanfilip.airsorsix.utills.generateId
+import com.ivanfilip.airsorsix.utills.loggerFor
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 import java.util.logging.Logger
+import javax.transaction.Transactional
 
 @Service
 class FlightService(val repository: FlightRepository) {
+    val logger = loggerFor<FlightService>()
+
+    @Transactional
+    fun addNewFlight(planeId: String, code: String, departureDateTime: LocalDateTime,
+                     arrivalDateTime: LocalDateTime, departureLocationId: String,
+                     arrivalLocationId: String, businessSeats: Int,
+                     economySeats: Int): Flight {
+        val flight = Flight(generateId(), planeId, code, )
+        logger.info("Saving flight [{}]", flight)
+        repository.save(flight)
+        return flight
+    }
 }
